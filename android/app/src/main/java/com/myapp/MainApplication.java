f95400aee0e33854a421fff1837ae95db31a73b7
tiagowl <winkellandi123@gmail.com>
2023-05-15 15:15:38 -0300

    config adicional firebase android

--- android/app/src/main/java/com/myapp/MainApplication.java
+++ android/app/src/main/java/com/myapp/MainApplication.java
@@ -14,6 +14,7 @@ import com.facebook.soloader.SoLoader;
 
 import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
 
 import java.util.List;
 
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
       protected List<ReactPackage> getPackages() {
         @SuppressWarnings("UnnecessaryLocalVariable")
         List<ReactPackage> packages = new PackageList(this).getPackages();
+        new ReactNativeFirebaseAppPackage()
         // Packages that cannot be autolinked yet can be added manually here, for example:
         // packages.add(new MyReactNativePackage());
         return packages;
